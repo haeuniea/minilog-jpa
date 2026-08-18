313aa39839ef8ae4f8788e9890bf2f9c4cb7424a
haeuniea <haeunhaa@gmail.com>
2026-08-18 14:36:53 +0900

    fix: DTO 필드 및 구조 수정

--- src/main/java/com/asdf/minilog/dto/FollowRequestDto.java
+++ src/main/java/com/asdf/minilog/dto/FollowRequestDto.java
@@ -1,9 +1,13 @@
 package com.asdf.minilog.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.NonNull;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class FollowRequestDto {
     @NonNull private Long followerId;
     @NonNull private Long followeeId;
